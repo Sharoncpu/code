ed70d3daebf6852965dcd32b2e7ee90cbae13db9
Sharoncpu <56985008+Sharoncpu@users.noreply.github.com>
2020-02-29 23:59:13 +0100

    Update DrawingApp.java

--- DrawingApp.java
+++ DrawingApp.java
@@ -1,20 +1,12 @@
 package org.sharon.spring;
 
-import org.springframework.context.support.AbstractApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class DrawingApp {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		
-		//Triangle triangle=new Triangle();
-		//triangle.draw();
+		system.out.println("hello people");
 
-		AbstractApplicationContext context=new ClassPathXmlApplicationContext("spring.xml");
-		context.registerShutdownHook();
-		Triangle triangle=(Triangle)context.getBean("parentTriangle");
-		triangle.draw();
 	}
 
 }
